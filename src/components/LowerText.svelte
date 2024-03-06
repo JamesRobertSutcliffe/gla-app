97c2bb0f515bb8497e294b8f9bee9f65c42cedb7
James Sutcliffe <jamessutcliffe2012@hotmail.co.uk>
2024-03-06 20:39:01 +0000

    landing page lower text component built

---
+++ src/components/LowerText.svelte
@@ -0,0 +1,13 @@
+<p class="poppins-medium">
+  I have developed this website to express my interest in the Software
+  Engineering Apprenticeship with the GLA data team. This application is
+  developed in Svelte, utilising MapLibre and D3.
+</p>
+
+<style>
+  p {
+    margin: 7rem 2rem 3.5rem 2rem;
+    padding: 2rem;
+    font-size: 1rem;
+  }
+</style>
