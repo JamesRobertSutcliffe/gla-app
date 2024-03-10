aad3a6e314d76dbc1118aee28ed2f6dee819b7c1
James Sutcliffe <jamessutcliffe2012@hotmail.co.uk>
2024-03-10 21:42:17 +0000

    markers object added to seperate doc.

---
+++ src/data/markers.js
@@ -0,0 +1,56 @@
+export const markers = [
+    {
+      lng: -0.0032223108945371023,
+      lat: 51.5779862577029,
+      popup: "Born in Whipps Cross Hospital, Leytonstone in 1991 and grew up in Waltham Forest",
+      color: "green",
+    },
+    {
+        lng: -0.104343146778515263,
+        lat:   51.53047330865305,
+        popup: "Attended City and Islington Sixth Form College",
+        color: "purple",
+      },
+    {
+      lng: -0.04079087191557936,
+      lat:  51.58637160000932,
+      popup: "Attended Willowfield School",
+      color: "blue",
+    },
+    {
+        lng: -0.1364863899380846,
+        lat:  51.513704238310886,
+        popup: "I have worked in various television and advertising studios in Soho as a video editor",
+        color: "pink",
+      },
+      {
+        lng: -0.07204288910610805,
+        lat:  51.563883592340765,
+        popup: "I lived in Stoke Newington, Hackney for a few years",
+        color: "yellow",
+      },
+      {
+        lng: -0.07541031609598803,
+        lat: 51.51487368226021,
+        popup: "I currently work for a media translation company called Transperfect in Aldgate",
+        color: "Orange",
+      },
+      {
+        lng: -0.10866811794150215,
+        lat:  51.56476537557143,
+        popup: "I learned a lot about coding with Founders and Coders at Space4 in Finsbury Park",
+        color: "red",
+      },
+      {
+        lng: -0.0929868443484065,
+        lat:  51.474025142193405,
+        popup: "I moved South of the River to Camberwell in 2018",
+        color: "indigo",
+      },
+      {
+        lng: -0.12839680238774012,
+        lat:  51.438255618730935,
+        popup: "I currently live in Steatham!",
+        color: "teal",
+      },
+  ];
