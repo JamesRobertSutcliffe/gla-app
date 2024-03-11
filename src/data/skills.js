80e27a308154829eeb0e62bf1384ba51cfb8aa44
James Sutcliffe <jamessutcliffe2012@hotmail.co.uk>
2024-03-11 19:36:07 +0000

    skills data added

---
+++ src/data/skills.js
@@ -0,0 +1,10 @@
+export default [
+    { ages: 'Vanilla JS', count: '20' },
+	{ ages: 'HTML', count: '15' },
+	{ ages: 'CSS', count: '15' },
+    { ages: 'Next.js', count: '15' },
+	{ ages: 'React', count: '10' },
+	{ ages: 'Node', count: '15' },
+	{ ages: 'Svelte', count: '10' },
+    { ages: 'SQL', count: '10' }
+];
