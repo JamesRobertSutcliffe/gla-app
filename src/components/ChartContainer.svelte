64e71cf6bf0cb808f80d22831f7cd90d7cb15edd
James Sutcliffe <jamessutcliffe2012@hotmail.co.uk>
2024-03-16 17:24:09 +0000

    aside contained rendered to page.

--- src/components/ChartContainer.svelte
+++ src/components/ChartContainer.svelte
@@ -1,6 +1,8 @@
 <script>
+  import Aside from "./Aside.svelte";
   import LineChart from "./LineChart.svelte";
   import { onMount } from "svelte";
+  import { skillTimeText } from "../data/text";
 
   export let type;
   export let chartProps;
@@ -27,40 +29,57 @@
   });
 </script>
 
-<h2>Development of my coding skills overtime.</h2>
-<section class="container">
-  {#if viewport === "desktop"}
-    <svelte:component
-      this={CHART_TYPES[type]}
-      {...chartProps}
-      chartWidth={800}
-      chartHeight={500}
-    />
-  {:else if viewport === "tablet"}
-    <svelte:component
-      this={CHART_TYPES[type]}
-      {...chartProps}
-      chartWidth={480}
-      chartHeight={375}
-    />
-  {:else}
-    <svelte:component
-      this={CHART_TYPES[type]}
-      {...chartProps}
-      chartWidth={window.innerWidth * 0.9}
-      chartHeight={window.innerWidth * 0.9 * 0.78}
-    />{/if}
-</section>
+<h2>Development of my coding skills over time.</h2>
+<div class="center">
+  <div class="width-50">
+    <Aside text={skillTimeText} />
+  </div>
+  <section class="container">
+    {#if viewport === "desktop"}
+      <svelte:component
+        this={CHART_TYPES[type]}
+        {...chartProps}
+        chartWidth={750}
+        chartHeight={500}
+      />
+    {:else if viewport === "tablet"}
+      <svelte:component
+        this={CHART_TYPES[type]}
+        {...chartProps}
+        chartWidth={480}
+        chartHeight={375}
+      />
+    {:else}
+      <svelte:component
+        this={CHART_TYPES[type]}
+        {...chartProps}
+        chartWidth={window.innerWidth * 0.9}
+        chartHeight={window.innerWidth * 0.9 * 0.78}
+      />{/if}
+  </section>
+</div>
 
 <style>
   .container {
-    max-width: 800px;
-    margin: auto;
+    max-width: 700px;
+    margin: 1rem;
   }
 
   h2 {
     text-align: center;
-    padding: 2rem;
-    margin: 2rem;
+    /* padding: 2rem; */
+    /* margin: 2rem; */
+  }
+
+  .center {
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: space-evenly;
+    align-items: center;
+    height: 75vh;
+  }
+
+  .width-50 {
+    width: 40%;
   }
 </style>
