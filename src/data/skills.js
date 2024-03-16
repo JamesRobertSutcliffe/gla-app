30a7d5f917a8212e882140f1d8b80539f4f5eb7a
James Sutcliffe <jamessutcliffe2012@hotmail.co.uk>
2024-03-16 13:18:42 +0000

    chart data imported

--- src/data/skills.js
+++ src/data/skills.js
@@ -2,9 +2,10 @@ export default [
     { ages: 'Vanilla JS', count: '20' },
 	{ ages: 'HTML', count: '15' },
 	{ ages: 'CSS', count: '15' },
-    { ages: 'Next.js', count: '15' },
-	{ ages: 'React', count: '10' },
-	{ ages: 'Node', count: '15' },
+    { ages: 'Next.js', count: '12.5' },
+	{ ages: 'React', count: '12.5' },
+	{ ages: 'Node', count: '10' },
 	{ ages: 'Svelte', count: '10' },
-    { ages: 'SQL', count: '10' }
+    { ages: 'SQL', count: '5' },
+	{ ages: 'C#', count: '5' },
 ];
